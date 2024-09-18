2a7a812f1b6a61c6ec0f2358bcc0d97c4d3c97e6
Matt Wang <matthework@hotmail.com>
2024-09-18 20:32:31 +1200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allow to accept json data for req.body
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
